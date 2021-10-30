9ec1e6dff17c84b90c323f76ed4fb61940fd12ce
jameschain <james.cordova423@gmail.com>
2021-10-29 21:04:46 -0400

    feat: close lottery

--- app.js
+++ app.js
@@ -16,24 +16,32 @@ const lotteryContract = new ethers.Contract(address, abi, provider).connect(
   wallet
 );
 
-const approve = () => {};
-
-const startLottery = () => {
+const startLottery = async () => {
   const currentDate = new Date();
-  const timestamp = Math.round(currentDate.getTime() / 1000) + 4 * 60 * 60;
+  const timestamp = Math.round(currentDate.getTime() / 1000) + 24 * 60 * 60;
 
-  approve();
+  console.log(timestamp);
 
-  lotteryContract
-    .startLottery(
-      timestamp, // Timestamp for future time
-      "5000000000000000000",
-      500,
-      [2500, 2000, 1250, 1750, 600, 1900],
-      2000
-    )
-    .then((data) => console.log("success", data))
-    .catch((e) => console.log("error", e));
+  const result = await lotteryContract.startLottery(
+    timestamp, // Timestamp for future time
+    "5000000000000000000",
+    500,
+    [2500, 2000, 1250, 1750, 600, 1900],
+    2000
+  );
+
+  console.log(result);
 };
 
+const closeLottery = async () => {
+  const result = await lotteryContract.closeLottery(3);
+  console.log(result);
+
+  const drawFinalResult =
+    await lotteryContract.drawFinalNumberAndMakeLotteryClaimable(3, true);
+
+  console.log(drawFinalResult);
+};
+
+// closeLottery();
 startLottery();
